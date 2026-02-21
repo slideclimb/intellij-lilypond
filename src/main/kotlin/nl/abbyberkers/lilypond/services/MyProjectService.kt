3eaaa167c48d0a03425a9c5da34d0933c55cbf6b
Abby Berkers <abbyberkers@gmail.com>
2026-02-21 20:43:59 +0100

    Setup plugin from template

--- src/main/kotlin/nl/abbyberkers/lilypond/services/MyProjectService.kt
+++ src/main/kotlin/nl/abbyberkers/lilypond/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package com.github.slideclimb.intellijlilypond.services
+package nl.abbyberkers.lilypond.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import com.github.slideclimb.intellijlilypond.MyBundle
+import nl.abbyberkers.lilypond.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
