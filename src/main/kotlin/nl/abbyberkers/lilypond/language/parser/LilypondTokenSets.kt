7997fde6ff7f3087c3ffedaa8fbeb94f8663920f
Abby Berkers <abbyberkers@gmail.com>
2026-03-07 13:45:45 +0100

    Parser improvements

--- src/main/kotlin/nl/abbyberkers/lilypond/language/parser/LilypondTokenSets.kt
+++ src/main/kotlin/nl/abbyberkers/lilypond/language/parser/LilypondTokenSets.kt
@@ -1,10 +1,11 @@
 package nl.abbyberkers.lilypond.language.parser
 
 import com.intellij.psi.tree.TokenSet
+import nl.abbyberkers.lilypond.language.psi.LilypondTypes
 
 interface LilypondTokenSets {
     companion object {
-        val COMMENTS = TokenSet.create()
+        val COMMENTS = TokenSet.create(LilypondTypes.BLOCK_COMMENT, LilypondTypes.LINE_COMMENT, LilypondTypes.SCM_BLOCK_COMMENT, LilypondTypes.SCM_LINE_COMMENT)
         val STRING_LITERALS = TokenSet.create()
     }
 }
